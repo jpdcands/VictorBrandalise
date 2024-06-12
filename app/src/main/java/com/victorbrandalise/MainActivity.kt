9918980cace4358b4e1db6c2899a8d1bd4c4e88f
Paul Doolittle <jpdcands211@gmail.com>
2024-06-12 05:58:22 -0400

    Initial Commit

--- app/src/main/java/com/victorbrandalise/MainActivity.kt
+++ app/src/main/java/com/victorbrandalise/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.filament.utils.Utils
+import com.victorbrandalise.ModelRenderer
 
 class MainActivity : ComponentActivity() {
+
     companion object {
         init {
             Utils.init()
